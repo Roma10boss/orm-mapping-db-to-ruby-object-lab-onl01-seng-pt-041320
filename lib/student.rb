f9623ec98911abe88b00ace38b57bd31712fd776
Romario Gustave <romagustave10@gmail.com>
2020-06-18 21:42:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,16 +94,12 @@ class Student
  end
 
  def self.first_X_students_in_grade_10(x)
-   sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE
-       grade = 10
-       LIMIT ?
-   SQL
-
-   s = DB[:conn].execute(sql, x)
- end
+     sql = <<-SQL
+        SELECT * FROM students WHERE grade = 10 LIMIT ?
+     SQL
+ 
+     s = DB[:conn].execute(sql, x)
+   end
 
 
  def self.first_student_in_grade_10
