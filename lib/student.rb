b93cf84a237d39363fc7e5971e9fea4b1ab47edc
Romario Gustave <romagustave10@gmail.com>
2020-06-18 21:40:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,10 +95,10 @@ class Student
 
  def self.first_X_students_in_grade_10
    sql = <<-SQL
-      SELECT * 
-      FROM students 
+      SELECT *
+      FROM students
       WHERE
-       grade = 10 
+       grade = 10
        LIMIT ?
    SQL
 
