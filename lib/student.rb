6b24e8cb2f684f01ed71df04a96d264f15518677
Romario Gustave <romagustave10@gmail.com>
2020-06-18 21:17:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,4 +72,14 @@ class Student
    end
  end
 
+ def self .students_below_12th_grade
+   sql = <<-SQL
+   SELECT COUNT(grade)
+   FROM students
+   WHERE grade <= 11
+   SQL
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+ end
+
 end
