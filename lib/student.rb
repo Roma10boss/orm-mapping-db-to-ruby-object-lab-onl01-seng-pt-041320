dd4807db62efc9b619619b21b28db4b0c509d6a3
Romario Gustave <romagustave10@gmail.com>
2020-06-18 21:20:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,13 @@ class Student
  end
  end
 
+ def self.all
+   sql = <<-SQL
+   SELECT *
+   FROM students
+   SQL
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+ end |
+
 end
